30560e7a7aeb938d0cac008439109ffbf3539ad1
nekonori <102738752+nekonori@users.noreply.github.com>
2022-05-24 17:33:05 +0530

    Update avl.cpp

--- 8/avl.cpp
+++ 8/avl.cpp
@@ -78,6 +78,78 @@ Node* insert(Node* node, int val){
     return node;
 }
 
+Node * minValueNode(Node* node)
+{
+    Node* current = node;
+
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+Node* deleteNode(Node* root, int key)
+{
+    if (root == NULL)
+        return root;
+    if ( key < root->val )
+        root->left = deleteNode(root->left, key);
+    else if( key > root->val )
+        root->right = deleteNode(root->right, key);
+    else
+    {
+        if( (root->left == NULL) ||
+            (root->right == NULL) )
+        {
+            Node *temp = root->left ?
+                         root->left :
+                         root->right;
+
+            if (temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else
+                *root = *temp;
+            free(temp);
+        }
+        else
+        {
+            Node* temp = minValueNode(root->right);
+            root->val = temp->val;
+            root->right = deleteNode(root->right,
+                                     temp->val);
+        }
+    }
+    if (root == NULL)
+        return root;
+    root->height = 1 + max(getHeight(root->left),
+                           getHeight(root->right));
+
+    int balance = getBalance(root);
+    if (balance > 1 &&
+        getBalance(root->left) >= 0)
+        return rightRotate(root);
+    if (balance > 1 &&
+        getBalance(root->left) < 0)
+    {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if (balance < -1 &&
+        getBalance(root->right) <= 0)
+        return leftRotate(root);
+    if (balance < -1 &&
+        getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    return root;
+}
+
 void printInorder(Node* node)
 {
     if (node == NULL)
@@ -87,16 +159,55 @@ void printInorder(Node* node)
     printInorder(node->right);
 }
 
+void searchFor(Node* node, int key){
+    if(!node){
+        cout<<"Could not find "<<key<<endl;
+        return;
+    }
+    if(node->val==key){
+        cout<<"Found "<<key<<endl;
+        return;
+    }
+    if(node->val<key)
+        searchFor(node->left, key);
+    else
+        searchFor(node->right, key);
+}
+
 int main(){
-    Node* root = insert(nullptr, 12);
-    root = insert(root, 34);
-    root = insert(root, 45);
-    root = insert(root, 1);
-    root = insert(root, 0);
-    root = insert(root, 89);
-    root = insert(root, 10);
-    root = insert(root, 25);
-    cout<<"Inorder traversal of the tree: \n";
-    printInorder(root);
+    int choice;
+    Node* root = nullptr;
+    while(true){
+        printf("0 for help >>");
+        cin>>choice;
+        if(choice == 0){
+            cout<<"1 -> insert\n";
+            cout<<"2 -> search\n";
+            cout<<"3 -> delete\n";
+            cout<<"4 -> print inorder\n";
+            cout<<"-1 -> exit\n";
+        } else if(choice == 1){
+            int val;
+            printf("Enter value to insert: ");
+            cin>>val;
+            root = insert(root, val);
+            printf("Done\n");
+        } else if(choice == 3){
+            int val;
+            printf("Enter value to delete: ");
+            cin>>val;
+            root = deleteNode(root, val);
+            printf("Done\n");
+        } else if(choice == 2){
+            int val;
+            printf("Enter value to search for: ");
+            cin>>val;
+            searchFor(root, val);
+        } else if(choice == 4){
+            printInorder(root);
+            cout<<endl;
+        }
+        else break;
+    }
     return 0;
 }
